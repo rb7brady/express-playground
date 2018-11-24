a30702f1d7d15d87904811b9a38e98a637afd343
rb7brady <rb7brady@gmail.com>
2018-11-23 19:44:15 -0600

    Initialize express project files

---
+++ routes/company.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var mysql      = require('mysql');
+/* GET users listing. */
+router.get('/:symbol', function(req, res, next) {
+    console.log("/Company hit");
+
+    var connection = mysql.createConnection({
+        host:"rtdb002.cppjlghnvwmg.us-east-2.rds.amazonaws.com",
+        user:"rtdb",
+        password:"Riptide99!",
+        database:"rtdb"
+    });
+
+    connection.connect();
+    var queryFilter = "\"" + req.params.symbol + "\"";
+    var queryBase = "SELECT * FROM company WHERE SYMBOL = ";
+    console.log("queryBase: " + queryBase);
+    var fullQuery = queryBase + queryFilter;
+    console.log(fullQuery);
+
+    connection.query(fullQuery, function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].name);
+        res.send(results);
+    });
+
+    connection.end();
+});
+
+module.exports = router;
