0201379bc84650db55f692c89a3fb75477c667c4
rb7brady <@92Hatbf1qaz>
2020-01-01 16:45:24 -0600

    Reworked methods into call back functions to force session id refresh to occur before anything else.
    Hardcoded output as HTML.

--- routes/publicAPI/smite/test.js
+++ routes/publicAPI/smite/test.js
@@ -4,112 +4,87 @@ var router = express.Router();
 var md5 = require('md5');
 var moment = require('moment');
 var constants = require('../../../_helpers/_constants/smite');
+var htmlHeaders = "<!doctype html>"+
+    "<html lang=\"en\">" +
+    "<head>" +
+    "  <meta charset=\"utf-8\">" +
+    "  <title>Smite Abilities</title>" +
+    "  <meta name=\"description\" content=\"Smite Ablities\">" +
+    "  <link rel=\"stylesheet\" href=\"css/styles.css?v=1.0\">" +
+    "<style>tr {valign:top;}</style>" +
+    "</head>" +
+    "<body>";
+var htmlFooters ="<script src=\"js/scripts.js\"></script></body></html>";
 
-router.get('/gods', gods);
-router.get('/gods/:godNames', gods);
-
-router.get('/session', session);
-
-var godNames = {
-    names:[
-        "Nu Wa",
-        "Aphrodite",
-        "Artio"
-    ]
-};
-
+var godNames = {names:[]};
 var devID = "1420";
 var authKey = "4F3F3D6041D24489AFB1AD5DB443E7E2";
 var sessionId = "B0542222BC5A4CF5B41AED50AD9280FD";
 var sessionTimestamp;
 
-function gods(req,res) {
-    var UTCDate = moment.utc().format('YYYYMMDDHHmmss');
-    var signature = md5(devID+constants.END_POINT.CREATESESSION+authKey+UTCDate);
-
-    var url = constants.BASE_URL +
-        constants.END_POINT.CREATESESSION +
-        constants.RESPONSE_TYPE.JSON + "/" +
-        devID +"/"+
-        signature +"/"+
-        UTCDate;
-    console.log("Invoking Smite API getgods Method: " +
-        "\n-sessionId: " + sessionId +
-        "\n-sessionTimestamp: " + sessionTimestamp);
-    console.log("Query Params: " + req.params);
-    console.log("Query Params: " + req.params.godNames);
+router.get('/gods', gods);
+router.get('/session', session);
+router.get('/gods/:godNames', requestSessionHandler, function (req,res) {
     godNames.names = req.params.godNames.split(',');
-    if(checkSessionExpired(sessionTimestamp)) {
-        //sessionTimestamp = moment.utc("20191222180302", "YYYYMMDDHHmmss");
-            //moment.utc().format('YYYYMMDDHHmmss');
-        console.log("session expired, creating new one.");
+    var endpoint = constants.BASE_URL + constants.END_POINT.GETGODS + constants.RESPONSE_TYPE.JSON;
+    requestHandler(req, res, endpoint, function(k) {formatGodsList(res, k)});
+});
 
-        request(url, options,
-            function (error, response, body) {
-                if (!error) {
-                    let session = JSON.parse(body);
-                    console.log(session.timestamp.toString());
-                    sessionId = session.session_id;
-                    sessionTimestamp = moment(session.timestamp.toString(), 'MM/DD/YYYY hh:mm:ss a').add(15, "minutes");
-                    console.log(sessionTimestamp.isAfter(moment.utc()));
-                    console.log("New Session ID: " + sessionId);
-                    console.log("New Session Expiration: " + sessionTimestamp);
-                } else {
-                    console.log('ERROR');
-                }
-            });
+function requestHandler(req, res, endpoint, callback) {
+    var UTCDate = moment.utc().format('YYYYMMDDHHmmss');
+    var signature = md5(devID+constants.END_POINT.GETGODS+authKey+UTCDate);
+    var url = endpoint + "/" + devID +"/"+ signature +"/"+ sessionId + "/" + UTCDate + "/" + constants.LANGUAGE_CODE.US;
+    request(url, function (error, response, body) {
+        if (!error) {
+            console.log(body);
+            callback(body);
+        }
+    });
+}
 
-    }
-
-    UTCDate = moment.utc().format('YYYYMMDDHHmmss');
-
-    signature = md5(devID+constants.END_POINT.GETGODS+authKey+UTCDate);
-     url = constants.BASE_URL +
-        constants.END_POINT.GETGODS +
-        constants.RESPONSE_TYPE.JSON + "/" +
-        devID +"/"+
-        signature +"/"+
-        sessionId + "/" +
-        UTCDate + "/" +
-        constants.LANGUAGE_CODE.US
-        ;
-
-    var options = {
-        method: 'GET',
-        url: url
-    };
-
-    console.log(url);
-    request(url, options,
-        function (error, response, body) {
+function requestSessionHandler(req, res, next) {
+    if (checkSessionExpired(sessionTimestamp)) {
+        var UTCDate = moment.utc().format('YYYYMMDDHHmmss');
+        var signature = md5(devID + constants.END_POINT.CREATESESSION + authKey + UTCDate);
+        var url = constants.BASE_URL+constants.END_POINT.CREATESESSION+constants.RESPONSE_TYPE.JSON+"/"+devID+"/"+signature+"/"+UTCDate;
+        request(url,function (error, response, body) {
             if (!error) {
-                //console.log(body);
-                let gods = JSON.parse(body);
-                //console.log('Number of gods: ' + gods.length);
-                var output = "";
-                for (i = 0; i<gods.length; i++) {
-                    for (j = 0; j < godNames.names.length; j++) {
-                        if (gods[i].Name == godNames.names[j]) {
-                            output += gods[i].Name.toUpperCase() + "==================";
-                            output += "\n\t1. " + createAbilityString(gods[i].Ability_1);
-                            output += "\t2. " + createAbilityString(gods[i].Ability_2);
-                            output += "\t3. " + createAbilityString(gods[i].Ability_3);
-                            output += "\tULT. " + createAbilityString(gods[i].Ability_4);
-                            output += "\tPASSIVE. " + createAbilityString(gods[i].Ability_5);
-                        }
-                    }
-                }
-                //console.log(output);
-                res.send(output);
+                let session = JSON.parse(body);
+                sessionId = session.session_id;
+                sessionTimestamp = moment(session.timestamp.toString(), 'MM/DD/YYYY hh:mm:ss a').add(15, "minutes");
+                next();
             }
         });
+    } else {
+        next();
+    }
+}
+
+function formatGodsList(res,body) {
+    var table  = "<table style=\"width:100%\">";
+    let jsonBody = JSON.parse(body);
+    var output = "";
+    for (i = 0; i<jsonBody.length; i++) {
+        for (j = 0; j < godNames.names.length; j++) {
+            if (jsonBody[i].Name == godNames.names[j]) {
+                output += "<tr></tr><th colspan='6'>" + jsonBody[i].Name.toUpperCase() + "</th></tr>";
+                output += "<tr valign='top'>";
+                output += createAbilityString(jsonBody[i].Ability_1);
+                output += createAbilityString(jsonBody[i].Ability_2);
+                output += createAbilityString(jsonBody[i].Ability_3);
+                output += createAbilityString(jsonBody[i].Ability_4);
+                output += createAbilityString(jsonBody[i].Ability_5);
+                output += "</tr>";
+            }
+        }
+    }
+    table += output + "</table>";
+    res.send(htmlHeaders + table + htmlFooters);
 }
 
 function checkSessionExpired() {
-    console.log("+entering checkSessionExpired." );
     if (sessionTimestamp != null) {
         if (sessionTimestamp.isAfter(moment.utc())) {
-            console.log("+Session not expired.");
             return false;
         }
     }
@@ -117,52 +92,17 @@ function checkSessionExpired() {
 }
 
 function createAbilityString(ability) {
-    var abilityString = "";
+    var abilityString = "<td>";
         abilityString += ability.Summary.toUpperCase();
     if (ability.Description.itemDescription.rankitems.length > 0) {
+        abilityString+="<dl>";
         for (k = 0; k < ability.Description.itemDescription.rankitems.length; k++) {
-            abilityString += "\n\t\t" + ability.Description.itemDescription.rankitems[k].description;
-            abilityString += "\n\t\t\t" + ability.Description.itemDescription.rankitems[k].value;
-
+            abilityString += "<dt style='color:blue'>" + ability.Description.itemDescription.rankitems[k].description + "</dt>";
+            abilityString += "<dt style='font:small-caption'>" + ability.Description.itemDescription.rankitems[k].value + "</dt>";
         }
+        abilityString+="</dl>";
     }
-    return abilityString += "\n";
-}
-//router.get('/:symbol', function(req, res, next) {
-function session(req,res, next) {
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    console.log('Entering session');
-
-    var signature = md5(devID+constants.END_POINT.CREATESESSION+authKey+UTCDate);
-    var url = constants.BASE_URL +
-        constants.END_POINT.CREATESESSION +
-        constants.RESPONSE_TYPE.JSON + "/" +
-        devID +"/"+
-        signature +"/"+
-        UTCDate;
-
-    var options = {
-        method: 'GET',
-        url: url
-    };
-    console.log(url);
-    request(url, options,
-        function (error, response, body) {
-        if (!error) {
-            let session = JSON.parse(body);
-            sessionId = session.session_id;
-            sessionTimestamp = session.timestamp.toString();
-            console.log("new session id: " + sessionId);
-            console.log("expires 15 minutes after: " + sessionTimestamp);
-        } else {
-            console.log('ERROR');
-        }
-        if (res != null) {
-            res.send();
-        }
-    });
+    return abilityString += "</td>";
 }
 
 module.exports = router;
